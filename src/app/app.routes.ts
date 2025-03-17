c08c812600301379a4f542a8f0776057c6b90c77
Laia Fuster <laia.fuster@dachser.com>
2025-03-17 14:31:50 +0100

    sdcalculator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
 import { MenuComponent } from './portal/menu/menu.component';
-import { DscalculatorComponent } from './portal/menu/dscalculator/dscalculator.component';
+import { SdcalculatorComponent } from './portal/menu/sdcalculator/sdcalculator.component';
 import { ScheduleComponent } from './portal/menu/schedule/schedule.component';
 import { KmdistancesComponent } from './portal/menu/kmdistances/kmdistances.component';
 import { LoginComponent } from './login/login.component';
@@ -16,40 +16,14 @@ export const routes: Routes = [
     {
         path: 'portal',
         component: PortalComponent,
-        /*  pathMatch: 'full', */
-        /* children: [
-            {
-                path: 'cstools',
-                component: MenuComponent,
-                children: [
-                    {
-                        path: 'sdcalculator',
-                        component: DscalculatorComponent,
-                    },
-                    {
-                        path: 'schedule',
-                        component: ScheduleComponent,
-                    },
-                    {
-                        path: 'kmdistances',
-                        component: KmdistancesComponent,
-                    },
-                    {
-                        path: '',
-                        redirectTo: 'sdcalculator',
-                        pathMatch: 'full'
-                    }
-                ]
-            },
-        ], */
-    },
+   },
     {
-        path: 'cstools',
+        path: 'portal/cstools',
         component: MenuComponent,
         children: [
             {
                 path: 'sdcalculator',
-                component: DscalculatorComponent,
+                component: SdcalculatorComponent,
             },
             {
                 path: 'schedule',
